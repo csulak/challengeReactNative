19d6a209a301b6df2760992d9d0f7753516440a0
facuchaves <facuchaves@gmail.com>
2020-07-06 17:01:54 -0300

    Separo los estilos de componente

---
+++ MyScreens/MyButtonStyles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  myButtonStyle: {
+    marginTop: 20,
+    width: 200,
+    height: 50,
+    backgroundColor: '#ff5204',
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 20,
+  },
+})
